58e35aa34ca0b0a6d7b912e21d35f80a9f4c2b3d
Robert Bulek <norbikxs@gmail.com>
2022-06-07 19:48:54 +0200

    Upload

---
+++ settings.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Settings:
+	def __init__(self):
+		self.width = 800
+		self.red = (255,0,0)
+		self.green = (0,255,0)
+		self.blue = (0,0,255)
+		self.yellow = (255,255,0)
+		self.white = (255,255,255)
+		self.black = (0,0,0)
+		self.purple = (128,0,128)
+		self.orange = (255,165,0)
+		self.grey = (128,128,128)
+		self.turq = (64,224,208)
